57cb36516d34cd39065ecf75c337b7d2c81eb919
Martin J <martin.jerabek1@gmail.com>
2021-11-20 17:52:26 +0100

    Final version with some comments

--- apifox.py
+++ apifox.py
@@ -2,14 +2,17 @@ import requests
 import ast
 import os.path
 
+#First API call. Response contains an img link + some other stuff
+print("Getting a link to cute fox...")
+resp = requests.get("https://randomfox.ca/floof") 
 
-resp = requests.get("https://randomfox.ca/floof")
-
+#Get the link and file name from the response and format the stuff
 respdict = ast.literal_eval(resp.text)
 imglink = respdict.get("image", "no link")
 imglink = imglink.replace("\\", "")
 filename = "foxy"+str(imglink.rsplit("/", 1)[1])
 
+#Downloading the actual picture
 imageresp = requests.get(imglink)
 
 if os.path.exists(filename):
@@ -21,3 +24,5 @@ else:
         print (f"Cute Foxy saved as {filename}")
         file.write(imageresp.content)
     
+print("Press anything to exit...")
+input()

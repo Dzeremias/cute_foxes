ab459a7b1f1a640235948dbfac389d7a9ca05db2
Martin Jeřábek <85935162+Dzeremias@users.noreply.github.com>
2022-04-22 09:23:59 +0200

    Add files via upload
    Version 2.0 with some user inputs and stuff.

--- apifox.py
+++ apifox.py
@@ -2,27 +2,31 @@ import requests
 import ast
 import os.path
 
-#First API call. Response contains an img link + some other stuff
-print("Getting a link to cute fox...")
-resp = requests.get("https://randomfox.ca/floof") 
-
-#Get the link and file name from the response and format the stuff
-respdict = ast.literal_eval(resp.text)
-imglink = respdict.get("image", "no link")
-imglink = imglink.replace("\\", "")
-filename = "foxy"+str(imglink.rsplit("/", 1)[1])
-
-#Downloading the actual picture
-imageresp = requests.get(imglink)
-
-if os.path.exists(filename):
-    print("This cute little foxy was already downloaded")
+while True:
+    userinp = input("How many foxes would you like to download? (q = quit): ")
+    if userinp == "q":
+        break
+    try:
+        foxnumber = int(userinp)
+    except ValueError:
+        print("Invalid input")
+        continue
     
-else:
-    print ("Cute Foxy downloaded!")
-    with open(filename, "wb") as file:
-        print (f"Cute Foxy saved as {filename}")
-        file.write(imageresp.content)
-    
-print("Press anything to exit...")
-input()
+    for i in range(foxnumber):
+        resp = requests.get("https://randomfox.ca/floof/")
+        respdict = ast.literal_eval(resp.text)
+        imglink = respdict.get("image", "no link")
+        imglink = imglink.replace("\\", "")
+        filename = "foxy"+str(imglink.rsplit("/", 1)[1])
+
+        imageresp = requests.get(imglink)
+
+        if os.path.exists(f"zapifox/{filename}"):
+            print(f"{filename} was already downloaded")
+            
+        else:
+            print ("New cute Foxy downloaded!")
+            with open(f"zapifox/{filename}", "wb") as file:
+                print (f"Cute Foxy saved as {filename}")
+                file.write(imageresp.content)
+        
